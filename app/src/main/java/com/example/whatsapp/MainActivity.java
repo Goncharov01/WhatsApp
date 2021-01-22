795d3f3c035c1f088f596c656350e2429916ef15
Goncharov01 <mr.sacha.goncharov.01@mail.ru>
2021-01-22 13:36:55 +0300

    Connection bottomNavView

--- app/src/main/java/com/example/whatsapp/MainActivity.java
+++ app/src/main/java/com/example/whatsapp/MainActivity.java
@@ -1,24 +1,73 @@
 package com.example.whatsapp;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import com.example.whatsapp.dashboardFragment.DashboardFragment;
+import com.example.whatsapp.homeFragment.HomeFragment;
+import com.example.whatsapp.navigationFragment.NavigationFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
+import java.util.Date;
+
 public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//      Commit one
-//      Commit two
+        bottomNavigationView = findViewById(R.id.nav_view);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener);
+
+    }
+
+    public BottomNavigationView.OnNavigationItemSelectedListener onNavigationItemSelectedListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment fragment;
+                    switch (item.getItemId()) {
+
+                        case R.id.navigation_home:
+                            fragment = new HomeFragment();
+                            loadFragment(fragment);
+                            return true;
+
+                        case R.id.navigation_dashboard:
+                            fragment = new DashboardFragment();
+                            loadFragment(fragment);
+                            return true;
+
+                        case R.id.navigation_notifications:
+                            fragment = new NavigationFragment();
+                            loadFragment(fragment);
+                            return true;
+                    }
+
+                    return false;
+                }
+            };
+
+    public void loadFragment(Fragment fragment) {
+        if (fragment != null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.container, fragment)
+                    .commit();
+        }
+
     }
 
 
